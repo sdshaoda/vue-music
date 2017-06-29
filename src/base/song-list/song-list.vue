a18c0c128ae06d9db2c3b4206dcbc6d6b34b67ed
sdshaoda <sdshaoda@126.com>
2017-06-29 21:18:09 +0800

    完成了歌手详情歌曲页的基本展示, 6.9

---
+++ src/base/song-list/song-list.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="song-list">
+    <ul>
+      <li class="item" v-for="(song, index) in songs" :key="index">
+        <div class="content">
+          <h2 class="name">{{song.name}}</h2>
+          <p class="desc">{{getDesc(song)}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  props: {
+    songs: {
+      type: Array,
+      default: []
+    }
+  },
+  methods: {
+    getDesc(song) {
+      return `${song.singer} · ${song.album}`
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import "~common/stylus/variable"
+@import "~common/stylus/mixin"
+
+.song-list
+  .item
+    display: flex
+    align-items: center
+    box-sizing: border-box
+    height: 64px
+    font-size: $font-size-medium
+    .content
+      flex: 1
+      line-height: 20px
+      overflow: hidden
+      .name
+        no-wrap()
+        color: $color-text
+      .desc
+        no-wrap()
+        margin-top: 4px
+        color: $color-text-d
+</style>
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,7 +1,6 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail">
-    </div>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
@@ -10,6 +9,7 @@ import { mapGetters } from 'vuex'
 import { getSingerDetail } from 'api/singer'
 import { ERR_OK } from 'api/config'
 import { createSong } from 'common/js/song'
+import MusicList from 'components/music-list/music-list'
 
 export default {
   data() {
@@ -18,6 +18,12 @@ export default {
     }
   },
   computed: {
+    title() {
+      return this.singer.name
+    },
+    bgImage() {
+      return this.singer.avatar
+    },
     ...mapGetters([
       'singer'
     ])
@@ -35,7 +41,6 @@ export default {
       getSingerDetail(this.singer.id).then((res) => {
         if (res.code === ERR_OK) {
           this.songs = this._normalizeSongs(res.data.list)
-          console.log(this.songs)
         }
       })
     },
@@ -50,21 +55,15 @@ export default {
       })
       return ret
     }
+  },
+  components: {
+    MusicList
   }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-  
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
 
   .slide-enter-active, .slide-leave-active
     transition: all 0.3s
