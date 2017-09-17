1a400034facb90733a4bfb5ad6bf4957b1fa3d62
sdshaoda <sdshaoda@126.com>
2017-09-17 15:50:32 +0800

    封面与歌词滑动切换

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -15,7 +15,7 @@
           <h2 class="subtitle" v-html="currentSong.singer"></h2>
         </div>
         <div class="middle" @touchstart.prevent="middleTouchStart" @touchmove.prevent="middleTouchMove" @touchend="middleTouchEnd">
-          <div class="middle-l">
+          <div class="middle-l" ref="middleL">
             <div class="cd-wrapper" ref="cdWrapper">
               <div class="cd" :class="cdClass">
                 <img :src="currentSong.image" alt="" class="image">
@@ -101,6 +101,8 @@ import ProgressBar from 'base/progress-bar/progress-bar'
 import ProgressCircle from 'base/progress-circle/progress-circle'
 
 const transform = prefixStyle('transform')
+// CSS属性变化的过渡时间
+const transitionDuration = prefixStyle('transitionDuration')
 
 export default {
   data() {
@@ -325,16 +327,49 @@ export default {
       const touch = e.touches[0]
       const deltaX = touch.pageX - this.touch.startX
       const deltaY = touch.pageY - this.touch.startY
-      // 如果是纵向滚动，不响应
+      // 不响应纵向滚动
       if (Math.abs(deltaY) > Math.abs(deltaX)) {
         return
       }
       const left = this.currentShow === 'cd' ? 0 : -window.innerWidth
-      const width = Math.min(0, Math.max(-window.innerWidth, left + deltaX))
-      this.$refs.lyricList.$el.style[transform] = `translate3d(${width}px, 0, 0)`
+      // offsetWidth 是歌词DOM到页面最左侧的距离 [-window.innerWidth, 0]
+      const offsetWidth = Math.min(0, Math.max(-window.innerWidth, left + deltaX))
+      // 滑动百分比
+      this.touch.percent = Math.abs(offsetWidth / window.innerWidth)
+      // translate3d 会开启GPU硬件加速
+      this.$refs.lyricList.$el.style[transform] = `translate3d(${offsetWidth}px, 0, 0)`
+      this.$refs.lyricList.$el.style[transitionDuration] = 0
+      this.$refs.middleL.style.opacity = 1 - this.touch.percent
+      this.$refs.middleL.style[transitionDuration] = 0
     },
     middleTouchEnd() {
-
+      let offsetWidth
+      let opacity
+      // 滑动比大于10% 切换
+      if (this.currentShow === 'cd') {
+        if (this.touch.percent > 0.1) {
+          offsetWidth = -window.innerWidth
+          opacity = 0
+          this.currentShow = 'lyric'
+        } else {
+          offsetWidth = 0
+          opacity = 1
+        }
+      } else {
+        if (this.touch.percent < 0.9) {
+          offsetWidth = 0
+          opacity = 1
+          this.currentShow = 'cd'
+        } else {
+          offsetWidth = -window.innerWidth
+          opacity = 0
+        }
+      }
+      this.$refs.lyricList.$el.style[transform] = `translate3d(${offsetWidth}px, 0, 0)`
+      this.$refs.lyricList.$el.style[transitionDuration] = '300ms'
+      this.$refs.middleL.style.opacity = opacity
+      this.$refs.middleL.style[transitionDuration] = '300ms'
+      this.touch.status = false
     },
     _pad(num, n = 2) {
       let len = num.toString().length
