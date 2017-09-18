ba65ff4ac6276208fed57d841bd33e0d306b528a
sdshaoda <sdshaoda@126.com>
2017-09-19 07:26:45 +0800

    歌单歌曲数据处理并应用

--- src/components/disc/disc.vue
+++ src/components/disc/disc.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-    <music-list :title="title" :bg-image="bgImage"></music-list>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
@@ -8,6 +8,7 @@
 import MusicList from 'components/music-list/music-list'
 import { getSongList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
+import { createSong } from 'common/js/song'
 import { mapGetters } from 'vuex'
 
 export default {
@@ -22,16 +23,37 @@ export default {
       'disc'
     ])
   },
+  data() {
+    return {
+      songs: []
+    }
+  },
   created() {
     this._getSongList()
   },
   methods: {
     _getSongList() {
+      if (!this.disc.dissid) {
+        // 返回推荐页
+        this.$router.push('/recommend')
+        return
+      }
       getSongList(this.disc.dissid).then((res) => {
         if (res.code === ERR_OK) {
-          console.log(res.cdlist[0].songlist)
+          // 格式化数据结构
+          this.songs = this._normalizeSongs(res.cdlist[0].songlist)
         }
       })
+    },
+    _normalizeSongs(list) {
+      let ret = []
+      list.forEach((musicData) => {
+        if (musicData.songid && musicData.albumid) {
+          // 使用工厂方法 createSong 生成歌曲的数据结构
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
     }
   },
   components: {
