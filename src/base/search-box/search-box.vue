18767198cd8a8c273cf59ae2c5fe53d83c167926
sdshaoda <sdshaoda@126.com>
2017-09-22 01:23:57 +0800

    新增搜索框search-box组件

---
+++ src/base/search-box/search-box.vue
@@ -0,0 +1,64 @@
+<template>
+  <!-- 搜索框 -->
+  <div class="search-box">
+    <i class="icon-search"></i>
+    <input type="text" class="box" :placeholder="placeholder" v-model="query">
+    <i class="icon-dismiss" v-show="query" @click="clear"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: '搜索歌曲、歌手'
+    }
+  },
+  data() {
+    return {
+      query: ''
+    }
+  },
+  created() {
+    this.$watch('query', (newQuery) => {
+      this.$emit('query', newQuery)
+    })
+  },
+  methods: {
+    clear() {
+      this.query = ''
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "~common/stylus/variable"
+
+.search-box
+  display: flex
+  align-items: center
+  box-sizing: border-box
+  width: 100%
+  padding: 0 6px
+  height: 40px
+  background: $color-highlight-background
+  border-radius: 6px
+  .icon-search
+    font-size: 24px
+    color: $color-background
+  .box
+    flex: 1
+    margin: 0 5px
+    line-height: 18px
+    background: $color-highlight-background
+    color: $color-text
+    font-size: $font-size-medium
+    outline: 0
+    &::placeholder
+      color: $color-text-d
+  .icon-dismiss
+    font-size: 16px
+    color: $color-background
+</style>
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,11 +1,69 @@
 <template>
-  <div>
-    搜索页面
+  <div class="search">
+    <div class="search-box-wrapper">
+      <search-box></search-box>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import SearchBox from 'base/search-box/search-box'
+
+export default {
+  components: {
+    SearchBox
+  }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+@import "~common/stylus/variable"
+@import "~common/stylus/mixin"
+
+.search
+  .search-box-wrapper
+    margin 20px
+  .shortcut-wrapper
+    position fixed
+    top 178px
+    bottom 0
+    width 100%
+    .shortcut
+      height 100%
+      overflow hidden
+      .hot-key
+        margin 0 20px 20px 20px
+        .title
+          margin-bottom 20
+          font-size $font-size-medium
+          color $color-text-l
+        .item
+          display inline-block
+          padding 5px 10px
+          margin 0 20px 10px 0
+          border-radius 6px
+          background $color-highlight-background
+          font-size $font-size-medium
+          color $color-text-d
+      .search-history
+        position relative
+        margin 0 20px
+        .title
+          display flex
+          align-items center
+          height 40px
+          font-size $font-size-medium
+          color $color-text-l
+          .text
+            flex: 1
+          .clear
+            extend-click()
+            .icon-clear
+              font-size $font-size-medium
+              color $color-text-d
+  .search-result
+    position fixed
+    width 100px
+    top 178px
+    bottom 0
 </style>
