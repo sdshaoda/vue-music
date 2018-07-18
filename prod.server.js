5529dbb014875300f346836c59f778565805b711
sdshaoda <sdshaoda@126.com>
2018-07-19 00:41:48 +0800

    Add node.js exress server

---
+++ prod.server.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const axios = require('axios')
+const bodyParser = require('body-parser')
+const config = require('./config/index')
+
+let app = express()
+
+let apiRoutes = express.Router()
+
+apiRoutes.get('/getDiscList', function (req, res) {
+  const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.error(e)
+  })
+})
+
+apiRoutes.get('/getCdInfo', function (req, res) {
+  const url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
+
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    let ret = response.data
+    if (typeof ret === 'string') {
+      let reg = /^\w+\(({.+})\)$/
+      let matches = ret.match(reg)
+      if (matches) {
+        ret = JSON.parse(matches[1])
+      }
+    }
+    res.json(ret)
+  }).catch((e) => {
+    console.error(e)
+  })
+})
+
+apiRoutes.get('/lyric', function (req, res) {
+  const url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
+
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    let ret = response.data
+    if (typeof ret === 'string') {
+      let reg = /^\w+\(({[^()]+})\)$/
+      let matches = ret.match(reg)
+      if (matches) {
+        ret = JSON.parse(matches[1])
+      }
+    }
+    res.json(ret)
+  }).catch((e) => {
+    console.error(e)
+  })
+})
+
+app.post('/api/getPurlUrl', bodyParser.json(), function (req, res) {
+  const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg'
+
+  console.log(req.body)
+  axios.post(url, req.body, {
+    headers: {
+      referer: 'https://y.qq.com/',
+      origin: 'https://y.qq.com',
+      'Content-type': 'application/x-www-form-urlencoded'
+    }
+  }).then((response) => {
+    console.log(response.data)
+    res.json(response.data)
+  }).catch((e) => {
+    console.error(e)
+  })
+})
+
+app.use('/api', apiRoutes)
+
+app.use(express.static('./dist'))
+
+const port = process.env.PORT || config.build.port
+
+module.exports = app.listen(port, (err) => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log(`Server running at http://127.0.0.1:${port}/`)
+})
