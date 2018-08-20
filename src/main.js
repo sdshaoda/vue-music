94e8f769852a3cfaa12df3e7ed5c17ab748ac113
dshao <dshao@i9i8.com>
2018-08-20 23:04:18 +0800

    添加vconsole便于线上调试

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import fastclick from 'fastclick'
 import VueLazyLoad from 'vue-lazyload'
+// import 'common/js/vconsole'
 
 import 'common/stylus/index.styl'
 
