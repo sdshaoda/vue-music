54c5e51349b7e01029856a33c70d103cd0ca3f59
sdshaoda <sdshaoda@126.com>
2018-11-03 20:02:54 +0800

    use require.ensure build chunkFileName

--- src/router/index.js
+++ src/router/index.js
@@ -4,53 +4,63 @@ import Router from 'vue-router'
 // 注册
 Vue.use(Router)
 
-const Recommend = (resolve) => {
-  import('components/recommend/recommend').then((module) => {
-    resolve(module)
-  })
-}
+// const Recommend = (resolve) => {
+//   import('components/recommend/recommend').then((module) => {
+//     resolve(module)
+//   })
+// }
+// const Recommend = () => import(/* webpackChunkName: "recommend" */ 'components/recommend/recommend')
 
-const Singer = (resolve) => {
-  import('components/singer/singer').then((module) => {
-    resolve(module)
-  })
-}
+// const Singer = (resolve) => {
+//   import('components/singer/singer').then((module) => {
+//     resolve(module)
+//   })
+// }
 
-const Rank = (resolve) => {
-  import('components/rank/rank').then((module) => {
-    resolve(module)
-  })
-}
+// const Rank = (resolve) => {
+//   import('components/rank/rank').then((module) => {
+//     resolve(module)
+//   })
+// }
 
-const Search = (resolve) => {
-  import('components/search/search').then((module) => {
-    resolve(module)
-  })
-}
+// const Search = (resolve) => {
+//   import('components/search/search').then((module) => {
+//     resolve(module)
+//   })
+// }
 
-const SingerDetail = (resolve) => {
-  import('components/singer-detail/singer-detail').then((module) => {
-    resolve(module)
-  })
-}
+// const SingerDetail = (resolve) => {
+//   import('components/singer-detail/singer-detail').then((module) => {
+//     resolve(module)
+//   })
+// }
 
-const Disc = (resolve) => {
-  import('components/disc/disc').then((module) => {
-    resolve(module)
-  })
-}
+// const Disc = (resolve) => {
+//   import('components/disc/disc').then((module) => {
+//     resolve(module)
+//   })
+// }
 
-const TopList = (resolve) => {
-  import('components/top-list/top-list').then((module) => {
-    resolve(module)
-  })
-}
+// const TopList = (resolve) => {
+//   import('components/top-list/top-list').then((module) => {
+//     resolve(module)
+//   })
+// }
 
-const UserCenter = (resolve) => {
-  import('components/user-center/user-center').then((module) => {
-    resolve(module)
-  })
-}
+// const UserCenter = (resolve) => {
+//   import('components/user-center/user-center').then((module) => {
+//     resolve(module)
+//   })
+// }
+
+const Recommend = r => require.ensure([], () => r(require('components/recommend/recommend')), 'recommend')
+const Singer = r => require.ensure([], () => r(require('components/singer/singer')), 'singer')
+const Rank = r => require.ensure([], () => r(require('components/rank/rank')), 'rank')
+const Search = r => require.ensure([], () => r(require('components/search/search')), 'search')
+const SingerDetail = r => require.ensure([], () => r(require('components/singer-detail/singer-detail')), 'singer-detail')
+const Disc = r => require.ensure([], () => r(require('components/disc/disc')), 'disc')
+const TopList = r => require.ensure([], () => r(require('components/top-list/top-list')), 'top-list')
+const UserCenter = r => require.ensure([], () => r(require('components/user-center/user-center')), 'user-center')
 
 export default new Router({
   mode: 'history',
